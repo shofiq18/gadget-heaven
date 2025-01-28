c44fa1e347c133c7d2885fe96e8c1b18c9e9fb5d
shofiq18 <shafiqulislam25021998@gmail.com>
2025-01-28 09:32:35 +0600

    added modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -75,7 +75,7 @@ const Cart = ({ cartList, cartDelete, clearCart }) => {
                     ))}
                 </div>
             )}
-
+{/* modal  */}
             {showModal && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-white p-8 rounded-lg shadow-lg text-center">
